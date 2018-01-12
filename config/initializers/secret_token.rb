f3d32295fccfe448b191a940a1482cda25a1cfff
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2018-01-12 23:46:54 +0530

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menazers::Application.config.secret_token = 'cbb25731f0a008e2629e16bb45d374006566bc4503a8c942239ac073189eaf39845da6f0d9b59943271db708b0cda73ed61178fe59117dea15ad689519c031cc'
